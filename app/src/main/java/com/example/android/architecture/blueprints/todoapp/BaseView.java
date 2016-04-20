8e3ee69c4070253f263a08fbfd5cff553f45633b
李照飞 <li.zhaofei15@xcar.com.cn>
2016-04-20 15:25:44 +0800

    完善MVP中View的注释。

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/BaseView.java
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/BaseView.java
@@ -19,6 +19,8 @@ package com.example.android.architecture.blueprints.todoapp;
 /**
  * MVP 中的View，View必须是哑View，即只能是显示数据，响应操作，除此之外没有任何权限了，
  * 不能够直接操作Model（当然也不能直接操作Model传递过来的Bean对象，一定要做到View只显示数据和响应操作）。
+ * 并且View中也要尽量去除所有逻辑操作，比如Activity的各个生命周期方法，以及Activity的
+ * onActivityResult(int requestCode, int resultCode, Intent data)方法都需要放到Presenter中托管和处理。
  */
 public interface BaseView<T> {
 
